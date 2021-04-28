2d2ef5e0e13fa5fe7b86a494b09bcf78dc57a612
Padraig O'Brien <pkobrien3254@eagle.fgcu.edu>
2021-04-28 05:55:38 -0400

    Jar Created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,50 +13,13 @@ public class Main extends Application {
 
   public static void main(String[] args) {
     launch(args);
-
-
-    final String JDBC_DRIVER = "org.h2.Driver";
-    final String DB_URL = "jdbc:h2:./res/HR";
-
-    //  Database credentials
-    final String USER = "";
-    final String PASS = "";
-    Connection conn = null;
-    Statement stmt = null;
-
-    try {
-      // STEP 1: Register JDBC driver
-      Class.forName(JDBC_DRIVER);
-
-      //STEP 2: Open a connection
-      conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-      //STEP 3: Execute a query
-      stmt = conn.createStatement();
-
-      String sql = "SELECT * FROM JOBS";
-
-      ResultSet rs = stmt.executeQuery(sql);
-      while (rs.next()) {
-        System.out.println(rs.getString(1));
-      }
-
-      // STEP 4: Clean-up environment
-      stmt.close();
-      conn.close();
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
   }
 
   @Override
 
   public void start(Stage primaryStage) throws Exception {
 
-    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
 
 
 
